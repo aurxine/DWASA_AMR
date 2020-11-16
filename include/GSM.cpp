d151f8f9f24874c7b67f94a0def7fddd03586dc2
aurxine <sarowardhrubo@gmail.com>
2020-11-16 12:48:51 +0600

    fixed a small issue

--- include/GSM.cpp
+++ include/GSM.cpp
@@ -1,4 +1,5 @@
 #include "GSM.h"
+#include<Arduino.h>
 
 GSM::GSM(uint8_t rx, uint8_t tx): SIM800L(rx, tx)
 {
@@ -38,7 +39,7 @@ String GSM::ReceiveMessage()
     {
         textMessage = SIM800L.readString();
         delay(100);
-        SIM800L.println("Received message = %s\r",textMessage);
+        Serial.println("Received message = %s\r" + textMessage);
     }
     return textMessage;
 }
